a2cc9ac863ef9bca8a5afb04c50d55e16b7506d4
adamarbain <u2101735@siswa.um.edu.my>
2025-05-30 22:58:15 +0800

    Add NativeWind and Tailwind CSS support to client-mobile
    - Introduced Babel and Metro configurations for NativeWind integration.
    - Created Tailwind CSS configuration file and added global styles.
    - Updated package-lock.json to include new dependencies: nativewind, tailwindcss, and react-native-bouncy-checkbox.
    - Added TypeScript definitions for NativeWind.
    - Refactored layout components and removed unused files to streamline the project structure.

---
+++ client-mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl text-blue-500 font-bold">Welcome to the app</Text>
+    </View>
+  );
+}
