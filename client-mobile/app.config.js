5ed97d0928b706795dfe8f85e06de85acc78cac6
adamarbain <u2101735@siswa.um.edu.my>
2026-01-05 13:58:25 +0800

    Bump app version to 1.0.3 in app.config.js and app.json

--- client-mobile/app.config.js
+++ client-mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "DengueEye",
     slug: "dengueeye-mobile-app",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/dengueeye_logo.png",
     scheme: "dengueeye",
